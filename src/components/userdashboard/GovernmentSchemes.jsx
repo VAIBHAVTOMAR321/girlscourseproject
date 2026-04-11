a2dd7fe6cfd5f0165d59f8ebd7a4aaf767673e31
Deepika <deepikachauhan8988@gmail.com>
2026-04-11 16:12:25 +0530

    updated

--- src/components/userdashboard/GovernmentSchemes.jsx
+++ src/components/userdashboard/GovernmentSchemes.jsx
@@ -384,7 +384,7 @@ const GovernmentSchemes = () => {
                                 display: 'flex',
                                 alignItems: 'center',
                                 justifyContent: 'center',
-                                backgroundColor: '#f5f5f5',
+                           
                                 borderBottom: '1px solid #e0e0e0'
                               }}>
                                 <img
@@ -461,7 +461,7 @@ const GovernmentSchemes = () => {
                                           display: 'flex',
                                           alignItems: 'center',
                                           justifyContent: 'center',
-                                          backgroundColor: '#f5f5f5',
+                                       
                                           borderBottom: '1px solid #e0e0e0'
                                         }}>
                                           <img
